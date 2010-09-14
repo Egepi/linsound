8dfc61751681315a2586282d50d47bf87db0c2b4
kc21989 <kc21989@bdff4289-c314-f86d-0eb1-63bb4a822091>
2010-09-14 16:31:06 +0000

    update source with comments for the differnt function calls

--- LinSound/LinSound/MusicPlayer.java
+++ LinSound/LinSound/MusicPlayer.java
@@ -15,21 +15,32 @@ public class MusicPlayer {
 		writeTo.println(toclient);
 	}
 	
+	/*
+	 * Plays this sound file from it's current location once till the end
+	 */
 	public void play() {
 		String toclient = "play#" + this.soundFile + "\0"; 
 		writeTo.println(toclient);		
 	}
 	
+	/*
+	 * Stop playing the sound and rewind it to the beginning
+	 */
 	public void stop() {
 		String toclient = "stop#" + this.soundFile + "\0";
 		writeTo.println(toclient);
 	}
-	
+	/*
+	 * Play the sound and continue looping it
+	 */
 	public void loop() {
 		String toclient = "loop#" + this.soundFile + "\0";
 		writeTo.println(toclient);
 	}
 	
+	/*
+	 * Pause the sound at it's current location
+	 */
 	public void pause() {
 		String toclient = "pause#" + this.soundFile + "\0";
 		writeTo.println(toclient);
